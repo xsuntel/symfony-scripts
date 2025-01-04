98c441ce9dcd878ea9bdf14a9347c219700d569e
Ryan Lim <xsuntel@naver.com>
2025-01-04 12:59:07 +0900

    initial commit

---
+++ scripts/linux/ubuntu/database/postgresql/_install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# ======================================================================================================================
+# Scripts - Linux - Ubuntu - PostgreSQL - Install
+# ======================================================================================================================
+# >>>> Platform
+if [ "${PLATFORM_TYPE}" == "Linux" ]; then
+
+  # --------------------------------------------------------------------------------------------------------------------
+  # PostgreSQL - Install the packages
+  # --------------------------------------------------------------------------------------------------------------------
+  # >>>> PostgreSQL
+  echo ">>>> PostgreSQL"
+  echo
+
+  local POSTGRESQL_STATUS
+  POSTGRESQL_STATUS=$(dpkg -l | grep -i "postgresql" | awk '{print $2}' | cut -d ':' -f1 | awk "/^postgresql$/")
+  if [ "${POSTGRESQL_STATUS}" != "postgresql" ]; then
+    sudo apt install -y postgresql
+    echo
+
+    sudo systemctl enable postgresql
+    echo
+  fi
+
+  # --------------------------------------------------------------------------------------------------------------------
+  # PostgreSQL - Update the configuration
+  # --------------------------------------------------------------------------------------------------------------------
+
+  # --------------------------------------------------------------------------------------------------------------------
+  # PostgreSQL - Check status
+  # --------------------------------------------------------------------------------------------------------------------
+
+  local POSTGRESQL_STATUS
+  POSTGRESQL_STATUS=$(systemctl is-active postgresql)
+  if [ "${POSTGRESQL_STATUS}" == "active" ]; then
+    sudo systemctl restart postgresql
+    echo
+  else
+    sudo systemctl start postgresql
+    echo
+  fi
+
+fi
---
+++ tools/console/twig.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# ======================================================================================================================
+# Scripts - Symfony - Console Commands - Twig
+# ======================================================================================================================
+
+PROJECT_PATH=$(dirname "$(dirname "$(dirname "$0")")")
+cd "${PROJECT_PATH}" || exit
+
+echo "---------------------------------------------------------------------------------------------------------------"
+echo "[ Symfony ] Console Commands - Twig"
+echo "---------------------------------------------------------------------------------------------------------------"
+echo
+
+  # >>>> Symfony Framework
+  if [ -d app ]; then
+    (
+      cd app || return
+
+      # >>>> Symfony Command                                             https://symfony.com/doc/current/deployment.html
+      if [ -f bin/console ]; then
+
+        # >>>> Select one of some environments
+        PS3="Menu: "
+        select num in "twig" "twig-component"; do
+          case "$REPLY" in
+          1)
+            CONSOLE_COMMANDS="twig"
+            break
+            ;;
+          2)
+            CONSOLE_COMMANDS="twig-component"
+            break
+            ;;
+          3)
+            echo "exit()"
+            exit
+            ;;
+          *)
+            echo "[ ERROR ] Unknown Command"
+            exit
+            ;;
+          esac
+        done
+        echo
+
+        # ----------------------------------------------------------------------------------------------------------------
+        # 1) twig
+        # ----------------------------------------------------------------------------------------------------------------
+        if [ "${CONSOLE_COMMANDS}" == "twig" ]; then
+          echo ">>>> twig"
+          php bin/console debug:twig
+
+        # ----------------------------------------------------------------------------------------------------------------
+        # 2) twig-component
+        # ----------------------------------------------------------------------------------------------------------------
+        elif [ "${CONSOLE_COMMANDS}" == "twig-component" ]; then
+          echo ">>>> twig-component"
+          php bin/console debug:twig-component
+
+        fi
+        echo
+
+      else
+        echo "[ ERROR ] There is not a command : app/bin/console"
+        exit
+      fi
+    )
+  else
+    echo "[ ERROR ] There is not a folder : app"
+    setExit
+  fi
+  echo
