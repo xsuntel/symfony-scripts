2bbdcf1bdf764d9d91ddc9c939215ebb58c8acb3
Ryan Lim <xsuntel@naver.com>
2025-04-08 22:49:34 +0900

    Updated some files

--- scripts/linux/ubuntu/server/_hosts.sh
+++ scripts/linux/ubuntu/server/_hosts.sh
@@ -10,7 +10,7 @@ if [ "${PLATFORM_TYPE}" == "Linux" ]; then
     # >>>> Hosts
     if [ "${ENVIRONMENT_NAME}" == "dev" ]; then
       local PROJECT_HOST_NAME
-      PROJECT_HOST_NAME=$(grep -i "symfony.local" /etc/hosts | awk '{print $2}' | head -n 1)
+      PROJECT_HOST_NAME=$(sudo grep -i "symfony.local" /etc/hosts | awk '{print $2}' | head -n 1)
       if [ ! "${PROJECT_HOST_NAME}" ]; then
         echo $'\n127.0.0.1 symfony.local' | sudo tee -a /etc/hosts
         echo $'\n127.0.0.1 api.symfony.local' | sudo tee -a /etc/hosts
