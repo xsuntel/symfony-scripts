cb56aab78ffde7dda22188f47933bec16963ac54
Ryan Lim <xsuntel@naver.com>
2025-06-03 00:34:01 +0900

    Updated some files

--- scripts/base/symfony/_server.sh
+++ scripts/base/symfony/_server.sh
@@ -3,69 +3,99 @@
 # Scripts - Base - Symfony - Local Web Server                  https://symfony.com/doc/current/setup/symfony_server.html
 # ======================================================================================================================
 
-# >>>> Environment
-if [ "${ENVIRONMENT_NAME}" == "dev" ]; then
+# >>>> App
+if [ -d app ]; then
+  (
+    cd app || return
 
-  # >>>> Platform
-  echo ">>>> PHP - Symfony - Local Web Server"
-  echo
+    # >>>> Symfony Framework
+    if [ -f bin/console ]; then
 
-  if [ -f .symfony.local.yaml ]; then
-    # ----------------------------------------------------------------------------------------------------------------
-    # Platform - Linux
-    # ----------------------------------------------------------------------------------------------------------------
-    if [ "${PLATFORM_TYPE}" == "Linux" ]; then
-      local LOCAL_LISTEN_PORT
-      LOCAL_LISTEN_PORT=$(netstat -an | grep -i LISTEN | grep -v tcp6 | grep ".8000" | awk '{print $6}')
-      if [ "${LOCAL_LISTEN_PORT}" == "LISTEN" ]; then
-        symfony local:server:stop
+      # >>>> Environment
+      if [ "${ENVIRONMENT_NAME}" == "dev" ]; then
+
+        # >>>> Platform
+        echo ">>>> PHP - Symfony - Local Web Server"
         echo
-      fi
-      # >>>> Symfony - Local Web Server - Enabling TLS
-      symfony server:ca:install
 
-    # ----------------------------------------------------------------------------------------------------------------
-    # Platform - MacOS
-    # ----------------------------------------------------------------------------------------------------------------
-    elif [ "${PLATFORM_TYPE}" == "Darwin" ]; then
-      local LOCAL_LISTEN_PORT
-      LOCAL_LISTEN_PORT=$(netstat -an | grep -i LISTEN | grep -v tcp6 | grep ".8000" | awk '{print $6}')
-      if [ "${LOCAL_LISTEN_PORT}" == "LISTEN" ]; then
-        symfony local:server:stop
-        echo
-      fi
-      # >>>> Symfony - Local Web Server - Enabling TLS
-      symfony server:ca:install
+        if [ -f ${PROJECT_PATH}/.symfony.local.yaml ]; then
+          cp -fv ${PROJECT_PATH}/.symfony.local.yaml ${PROJECT_PATH}/app/.symfony.local.yaml
+        fi
 
-    # ----------------------------------------------------------------------------------------------------------------
-    # Platform - Windows
-    # ----------------------------------------------------------------------------------------------------------------
-    elif [ "${PLATFORM_TYPE}" == "Windows" ]; then
-      local LOCAL_LISTEN_PORT
-      LOCAL_LISTEN_PORT=$(netstat -an | grep -i LISTEN | grep -v tcp6 | grep "8000" | awk '{print $6}')
-      if [ "${LOCAL_LISTEN_PORT}" == "LISTEN" ]; then
-        symfony local:server:stop
-        echo
+        if [ -f ${PROJECT_PATH}/app/.symfony.local.yaml ]; then
+          # ------------------------------------------------------------------------------------------------------------
+          # Platform - Linux
+          # ------------------------------------------------------------------------------------------------------------
+          if [ "${PLATFORM_TYPE}" == "Linux" ]; then
+            local LOCAL_LISTEN_PORT
+            LOCAL_LISTEN_PORT=$(netstat -an | grep -i LISTEN | grep -v tcp6 | grep ".8080" | awk '{print $6}')
+            if [ "${LOCAL_LISTEN_PORT}" == "LISTEN" ]; then
+              symfony local:server:stop
+              echo
+            fi
+            # >>>> Symfony - Local Web Server - Enabling TLS
+            symfony server:ca:install
+
+          # ------------------------------------------------------------------------------------------------------------
+          # Platform - MacOS
+          # ------------------------------------------------------------------------------------------------------------
+          elif [ "${PLATFORM_TYPE}" == "Darwin" ]; then
+            local LOCAL_LISTEN_PORT
+            LOCAL_LISTEN_PORT=$(netstat -an | grep -i LISTEN | grep -v tcp6 | grep ".8080" | awk '{print $6}')
+            if [ "${LOCAL_LISTEN_PORT}" == "LISTEN" ]; then
+              symfony local:server:stop
+              echo
+            fi
+            # >>>> Symfony - Local Web Server - Enabling TLS
+            symfony server:ca:install
+
+          # ------------------------------------------------------------------------------------------------------------
+          # Platform - Windows
+          # ------------------------------------------------------------------------------------------------------------
+          elif [ "${PLATFORM_TYPE}" == "Windows" ]; then
+            local LOCAL_LISTEN_PORT
+            LOCAL_LISTEN_PORT=$(netstat -an | grep -i LISTEN | grep -v tcp6 | grep "8080" | awk '{print $6}')
+            if [ "${LOCAL_LISTEN_PORT}" == "LISTEN" ]; then
+              symfony local:server:stop
+              echo
+            fi
+            # >>>> Symfony - Local Web Server - Enabling TLS
+            symfony server:ca:install
+
+          else
+            echo "Please check Operating System"
+            setExit
+          fi
+
+          # >>>> Symfony - Local Web Server
+          symfony local:server:start
+          #symfony local:server:start --no-tls --no-workers --daemon
+
+          symfony server:ls
+          echo
+          symfony server:status
+
+        else
+          echo "[ ERROR ] There is not a file : .symfony.local.yaml"
+          settExit
+        fi
+
+      else
+
+        if [ -f ${PROJECT_PATH}/app/.symfony.local.yaml ]; then
+          rm -f ${PROJECT_PATH}/app/.symfony.local.yaml
+        fi
+
       fi
-      # >>>> Symfony - Local Web Server - Enabling TLS
-      symfony server:ca:install
+      echo
 
     else
-      echo "Please check Operating System"
+      echo "[ ERROR ] There is not a command : app/bin/console"
       setExit
     fi
-
-    # >>>> Symfony - Local Web Server
-    symfony local:server:start
-    #symfony local:server:start --no-tls --no-workers --daemon
-
-    symfony server:ls
-    echo
-    symfony server:status
-
-  else
-    echo "[ ERROR ] There is not a file : .symfony.local.yaml"
-    settExit
-  fi
+  )
 fi
 echo
+
+
+
