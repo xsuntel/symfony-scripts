5325b415fb8a12142dd48304fb0767ecb3e9c1eb
Ryan Lim <xsuntel@naver.com>
2025-06-03 00:56:56 +0900

    Updated some files

--- scripts/base/symfony/_server.sh
+++ scripts/base/symfony/_server.sh
@@ -18,58 +18,62 @@ if [ -d app ]; then
         echo ">>>> PHP - Symfony - Local Web Server"
         echo
 
+        # ------------------------------------------------------------------------------------------------------------
+        # Platform - Linux
+        # ------------------------------------------------------------------------------------------------------------
+        if [ "${PLATFORM_TYPE}" == "Linux" ]; then
+          local LOCAL_LISTEN_PORT
+          LOCAL_LISTEN_PORT=$(netstat -an | grep -i LISTEN | grep -v tcp6 | grep ".8080" | awk '{print $6}')
+          if [ "${LOCAL_LISTEN_PORT}" == "LISTEN" ]; then
+            symfony local:server:stop
+            echo
+          fi
+
+          # >>>> Symfony - Local Web Server - Enabling TLS
+          if [! -f ~/.symfony5/certs/default.p12 ]; then
+            symfony server:ca:install
+          fi
+
+        # ------------------------------------------------------------------------------------------------------------
+        # Platform - MacOS
+        # ------------------------------------------------------------------------------------------------------------
+        elif [ "${PLATFORM_TYPE}" == "Darwin" ]; then
+          local LOCAL_LISTEN_PORT
+          LOCAL_LISTEN_PORT=$(netstat -an | grep -i LISTEN | grep -v tcp6 | grep ".8080" | awk '{print $6}')
+          if [ "${LOCAL_LISTEN_PORT}" == "LISTEN" ]; then
+            symfony local:server:stop
+            echo
+          fi
+
+          # >>>> Symfony - Local Web Server - Enabling TLS
+          symfony server:ca:install
+
+        # ------------------------------------------------------------------------------------------------------------
+        # Platform - Windows
+        # ------------------------------------------------------------------------------------------------------------
+        elif [ "${PLATFORM_TYPE}" == "Windows" ]; then
+          local LOCAL_LISTEN_PORT
+          LOCAL_LISTEN_PORT=$(netstat -an | grep -i LISTEN | grep -v tcp6 | grep "8080" | awk '{print $6}')
+          if [ "${LOCAL_LISTEN_PORT}" == "LISTEN" ]; then
+            symfony local:server:stop
+            echo
+          fi
+
+          # >>>> Symfony - Local Web Server - Enabling TLS
+          symfony server:ca:install
+
+        else
+          echo "Please check Operating System"
+          setExit
+        fi
+
+        # >>>> Symfony - Local Web Server
         if [ -f ${PROJECT_PATH}/.symfony.local.yaml ]; then
           cp -fv ${PROJECT_PATH}/.symfony.local.yaml ${PROJECT_PATH}/app/.symfony.local.yaml
         fi
 
         if [ -f ${PROJECT_PATH}/app/.symfony.local.yaml ]; then
-          # ------------------------------------------------------------------------------------------------------------
-          # Platform - Linux
-          # ------------------------------------------------------------------------------------------------------------
-          if [ "${PLATFORM_TYPE}" == "Linux" ]; then
-            local LOCAL_LISTEN_PORT
-            LOCAL_LISTEN_PORT=$(netstat -an | grep -i LISTEN | grep -v tcp6 | grep ".8080" | awk '{print $6}')
-            if [ "${LOCAL_LISTEN_PORT}" == "LISTEN" ]; then
-              symfony local:server:stop
-              echo
-            fi
-            # >>>> Symfony - Local Web Server - Enabling TLS
-            symfony server:ca:install
-
-          # ------------------------------------------------------------------------------------------------------------
-          # Platform - MacOS
-          # ------------------------------------------------------------------------------------------------------------
-          elif [ "${PLATFORM_TYPE}" == "Darwin" ]; then
-            local LOCAL_LISTEN_PORT
-            LOCAL_LISTEN_PORT=$(netstat -an | grep -i LISTEN | grep -v tcp6 | grep ".8080" | awk '{print $6}')
-            if [ "${LOCAL_LISTEN_PORT}" == "LISTEN" ]; then
-              symfony local:server:stop
-              echo
-            fi
-            # >>>> Symfony - Local Web Server - Enabling TLS
-            symfony server:ca:install
-
-          # ------------------------------------------------------------------------------------------------------------
-          # Platform - Windows
-          # ------------------------------------------------------------------------------------------------------------
-          elif [ "${PLATFORM_TYPE}" == "Windows" ]; then
-            local LOCAL_LISTEN_PORT
-            LOCAL_LISTEN_PORT=$(netstat -an | grep -i LISTEN | grep -v tcp6 | grep "8080" | awk '{print $6}')
-            if [ "${LOCAL_LISTEN_PORT}" == "LISTEN" ]; then
-              symfony local:server:stop
-              echo
-            fi
-            # >>>> Symfony - Local Web Server - Enabling TLS
-            symfony server:ca:install
-
-          else
-            echo "Please check Operating System"
-            setExit
-          fi
-
-          # >>>> Symfony - Local Web Server
           symfony local:server:start
-          #symfony local:server:start --no-tls --no-workers --daemon
 
           symfony server:ls
           echo
